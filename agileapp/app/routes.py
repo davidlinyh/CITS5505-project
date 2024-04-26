dbe137b1bbeb47365e1f24dc12309d7759a6f568
davidlinyh <davidlinyh@outlook.com>
2024-04-26 16:57:35 +0800

    added basic html for login and register page

--- agileapp/app/routes.py
+++ agileapp/app/routes.py
@@ -1,7 +1,23 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Login code here
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # Process registration form here
+        # Validate form data, create user, etc.
+        return redirect(url_for('login'))  # Redirect to login after registration
+    return render_template('register.html')
+                           
