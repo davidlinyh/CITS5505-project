3579002f38690fb07593aded3695b6249eb63b3a
davidlinyh <davidlinyh@outlook.com>
2024-05-11 09:36:30 +0800

    Revert "Merge branch 'main' into css-changes"
    This reverts commit 1559fc559692d0a4bec3708d459799c55b6c4796, reversing
    changes made to 6a36785572c759082facc74161de20a54ce9beea.

--- agileapp/app/models.py
+++ agileapp/app/models.py
@@ -1,9 +1,7 @@
 from datetime import datetime, timezone
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
-from app import db, login
+from app import db
 
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
@@ -11,21 +9,11 @@ class User(UserMixin, db.Model):
     first_name = db.Column(db.String(128))
     last_name = db.Column(db.String(128))
     photo_path = db.Column(db.String(128))
-    created_at = db.Column(db.DateTime, default=datetime.now())
-    updated_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
-    
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-    
-@login.user_loader
-def load_user(id):
-    return db.session.get(User, int(id))
     
 class LostItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- agileapp/app/routes.py
+++ agileapp/app/routes.py
@@ -1,10 +1,5 @@
-from urllib.parse import urlsplit
-from flask import render_template, request, redirect, url_for, flash
-from flask_login import current_user, login_user, logout_user, login_required
-import sqlalchemy as sa
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
-from app.models import User
+from flask import render_template, request, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
@@ -13,61 +8,28 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    print('login')
-    if current_user.is_authenticated:
+    if request.method == 'POST':
+        # Login code here
         return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        # print('enter validate')
-        user = db.session.scalar(sa.select(User).where(User.email == form.email.data))
-        if user is None:
-            register_link = '<a href="{}">register</a>'.format(url_for('register'))
-            flash('User not found. Please {} first.'.format(register_link), 'info')
-            return redirect(url_for('login'))
-        elif not user.check_password(form.password.data):
-            flash('Invalid password')
-            return redirect(url_for('login'))
-
-        login_user(user)
-
-        return redirect(url_for('gallery'))
-    return render_template('login.html', title="Log In", form=form)
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
+    return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    print('enter register')
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        print("enter validation")
-        user = User(first_name=form.firstname.data, last_name=form.lastname.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Registration Success! Please Log in to continue.')
-        return redirect(url_for('login'))
-    print(form.data)
-    print(form.errors)
-    return render_template('register.html', title='Register', form=form)
+    if request.method == 'POST':
+        # Process registration form here
+        # Validate form data, create user, etc.
+        return redirect(url_for('login'))  # Redirect to login after registration
+    return render_template('register.html')
 
 @app.route('/gallery')
-@login_required
 def gallery():
     return render_template('gallery.html')
 
 @app.route('/account')
-@login_required
 def account():
     return render_template('account.html')
 
 @app.route('/manage-account', methods=['GET', 'POST'])
-@login_required
 def manage_account():
     if request.method == 'POST':
         # Process changes here
@@ -75,7 +37,6 @@ def manage_account():
     return render_template('manage-account.html')
 
 @app.route('/item')
-@login_required
 def item():
     return render_template('item.html')
 
