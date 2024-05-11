30fbc4d440b8a74421d3cb05e01f7c4877b4e4c0
Ida Bagus Prawira Janottama KN <23894575@student.uwa.edu.au>
2024-05-11 21:21:14 +0800

    admin add lost item

--- agileapp/app/routes.py
+++ agileapp/app/routes.py
@@ -2,9 +2,12 @@ from urllib.parse import urlsplit
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
+from werkzeug.utils import secure_filename
+from datetime import datetime
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, AddItemForm
+from app.models import User, LostItem
+import os
 
 @app.route('/')
 @app.route('/index')
@@ -75,22 +78,47 @@ def manage_account():
 
 @app.route('/item/<int:item_id>')
 @login_required
+@login_required
 def item(item_id):
     item = LostItem.query.get_or_404(item_id)  # Fetch the item or return 404 if not found
     return render_template('item.html', item=item)
 
 @app.route('/admin/index')
 @app.route('/admin')
+@login_required
 def admin_index():
     return render_template('admin/index.html')
 
 @app.route('/admin/manage-items', methods=['GET']) 
+@login_required
 def admin_manage_items(): 
     return render_template('admin/manage-items.html')
 
 @app.route('/admin/new-item', methods=['GET', 'POST'])
+@login_required
 def new_item():
-    if request.method == 'POST':
-        # Process new item form here
-        return redirect(url_for('admin_manage_item'))
-    return render_template('admin/new-item.html')
+    form = AddItemForm()
+    if form.validate_on_submit():
+        print("enter validation")
+        item = LostItem(name=form.name.data, 
+                        description=form.description.data, 
+                        tags="default_tags", 
+                        photo_paths="", 
+                        admin_id=current_user.id)
+        files = request.files.getlist('photos')
+        if files:
+            for index, file in enumerate(files):
+                if index > 0:
+                    item.photo_paths = item.photo_paths + ','
+                timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+                filename = secure_filename(timestamp+'_'+str(index))
+                file_path = os.path.join(app.config['ITEM_PHOTO_FOLDER'], filename)
+                file.save(file_path)
+                item.photo_paths = item.photo_paths + file_path
+
+        db.session.add(item)
+        db.session.commit()
+
+        return redirect(url_for('admin_manage_items'))
+
+    return render_template('admin/new-item.html', form=form)
