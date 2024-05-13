4fabb18b8ad883e771f0ab0f7094422bd1230c4e
Adharsh Sundaram Soudakar <23796349@student.uwa.edu.au>
2024-05-13 11:59:35 +0800

    Added a function to config.py that generates a random secret key

--- agileapp/app/config.py
+++ agileapp/app/config.py
@@ -1,8 +1,11 @@
 import os
+import secrets
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+def generate_secureKey(length=50):
+    return secrets.token_hex(length)
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or generate_secureKey()
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
