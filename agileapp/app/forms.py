fc6ec2d5100fc14ddac02f47a11b0a5404040c79
Adharsh Sundaram Soudakar <23796349@student.uwa.edu.au>
2024-05-17 07:49:25 +0800

    Implemented secretkey generator, escaped html special characters in input fields, implemented password length and special character check

--- agileapp/app/forms.py
+++ agileapp/app/forms.py
@@ -1,12 +1,20 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField, SelectField, TextAreaField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 import sqlalchemy as sa
 from app import db
 from app.models import User
+import re
 
-
+def checkNames(form,field):
+    if not re.match("^[a-zA-Z ]*$",field.data):
+        raise ValidationError("Invalid characters. Use only characters and whitespaces.")
+    
+def checkPassword(form,field):
+    if not re.search("[!@#$%^&*(),.?:}{\"|<>]",field.data):
+        raise ValidationError("Password should contain atleast one special character")
+    
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -14,10 +22,10 @@ class LoginForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
-    firstname = StringField('First Name', validators=[DataRequired()])
-    lastname = StringField('Last Name', validators=[DataRequired()])
+    firstname = StringField('First Name', validators=[DataRequired(), checkNames()])
+    lastname = StringField('Last Name', validators=[DataRequired(), checkNames()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8,max=30),checkPassword()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
